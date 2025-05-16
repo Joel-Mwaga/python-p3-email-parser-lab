7c2a6e7112affaa6cd2765eb31ce8391400cbe84
Joel Mwaga <mwagahjoel14@gmail.com>
2025-05-17 01:19:00 +0300

    changed 1 file

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,14 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, emails):
+        self.emails = emails
+
+    def parse(self):
+        # Find all email addresses using regex
+        found = re.findall(r'\b[\w\.-]+@[\w\.-]+\.\w+\b', self.emails)
+        # Remove duplicates and sort
+        return sorted(set(found))
+
+def test_hello_world():
+    assert True
